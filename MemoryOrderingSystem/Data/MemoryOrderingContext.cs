bf7d2bb1ad4d5b8a178c50790af547bad342d559
Otavio Paes <otaviopaesl@outlook.com>
2020-12-06 14:14:57 -0300

    Initial Commit

---
+++ MemoryOrderingSystem/Data/MemoryOrderingContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoryOrderingSystem.Data
+{
+    public class MemoryOrderingContext : DbContext
+    {
+        public MemoryOrderingContext(DbContextOptions<MemoryOrderingContext> options) : base(options)
+        {
+        }
+    }
+}
